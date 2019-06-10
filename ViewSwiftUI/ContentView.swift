f102d97493c9cf53fbe89a07f1579713f9d25459
MACJaveloper <mac.javeloper@gmail.com>
2019-06-10 11:26:20 -0500

    Initial Commit

---
+++ ViewSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ViewSwiftUI
+//
+//  Created by CEDAM11 on 6/10/19.
+//  Copyright © 2019 CEDAM11. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
